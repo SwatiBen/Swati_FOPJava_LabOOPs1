02aa2bc5c840dc7a450b410e2692bc177efe149f
Swati Beniwal <101699580+SwatiBen@users.noreply.github.com>
2023-08-15 22:19:05 +0530

    Update Graded_Lab_4_DBMS.sql

--- Graded_Lab_4_DBMS.sql
+++ Graded_Lab_4_DBMS.sql
@@ -1,4 +1,4 @@
-/* 1 Create the tables. */
+/* 1 & 2 - Creating the tables. */
 
 Create Database if not exists `ecomm_DBMS` ;
 use `ecomm_DBMS`;
@@ -65,49 +65,82 @@ FOREIGN KEY (`SUPP_ID`) REFERENCES supplier (`SUPP_ID`),
 FOREIGN KEY (`CUS_ID`) REFERENCES customer(`CUS_ID`)
 );
 
-/* 2 Insert values in all the tables */
+/* 3 Insert values in all the tables */
 
-insert into `supplier` values(1, "Rajesh Retails", "Delhi", "1234567890");
-insert into `supplier` values(2, "Appario Ltd.", "Mumbai", "2589631470");
-insert into `supplier` values(3, "Knome products", "Banglore", "9785462315");
-insert into `supplier` values(4, "Bansal Retails", "Kochi", "8975463285");
-insert into `supplier` values(5, "Mittal Ltd.", "Lucknow", "7898456532");
+INSERT INTO `supplier` (`supp_id`, `supp_name`, `supp_city`, `supp_phone`) VALUES
+(1, 'Rajesh Retails', 'Delhi', '1234567890'),
+(2, 'Appario Ltd.', 'Mumbai', '2589631470'),
+(3, 'Knome products', 'Banglore', '9785462315'),
+(4, 'Bansal Retails', 'Kochi', '8975463285'),
+(5, 'Mittal Ltd.', 'Lucknow', '7898456532');
 
-insert into `customer` values(1, "AAKASH", "9999999999", "DELHI", "M");
-insert into `customer` values(2, "AMAN", "9785463215", "NOIDA", "M");
-insert into `customer` values(3, "NEHA", "9999999999", "MUMBAI", "F");
-insert into `customer` values(4, "MEGHA", "9994562399", "KOLKATA", "F");
-insert into `customer` values(5, "PULKIT", "7895999999", "LUCKNOW", "M");
+INSERT INTO `customer` (`cus_id`, `cus_name`, `cus_phone`, `cus_city`, `cus_gender`) VALUES
+(1, 'AAKASH', '9999999999', 'DELHI', 'M'),
+(2, 'AMAN', '9785463215', 'NOIDA', 'M'),
+(3, 'NEHA', '9999999999', 'MUMBAI', 'F'),
+(4, 'MEGHA', '9994562399', 'KOLKATA', 'F'),
+(5, 'PULKIT', '7895999999', 'LUCKNOW', 'M');
 
-insert into `category` values(1, "BOOKS"); 
-insert into `category` values(2, "GAMES"); 
-insert into `category` values(3, "GROCERIES"); 
-insert into `category` values(4, "ELECTRONICS"); 
-insert into `category` values(5, "CLOTHES");
 
-insert into `product` values(1, "GTA V", "DFJDJFDJFDJFDJFJF", 2);
-insert into `product` values(2, "TSHIRT", "DFDFJDFJDKFD", 5);
-insert into `product` values(3, "ROG LAPTOP", "DFNTTNTNTERND", 4);
-insert into `product` values(4, "OATS", "REURENTBTOTH", 3);
-insert into `product` values(5, "HARRY POTTER", "NBEMCTHTJTH", 1);
+INSERT INTO `category` (`cat_id`, `cat_name`) VALUES
+(1, 'BOOKS'),
+(2, 'GAMES'),
+(3, 'GROCERIES'),
+(4, 'ELECTRONICS'),
+(5, 'CLOTHES'),
+(6, 'SPORTS');
 
-insert into `supplier_pricing` values(1, 1, 2, 1500);
-insert into `supplier_pricing` values(2, 3, 5, 30000);
-insert into `supplier_pricing` values(3, 5, 1, 3000);
-insert into `supplier_pricing` values(4, 2, 3, 2500);
-insert into `supplier_pricing` values(5, 4, 1, 1000);
+INSERT INTO `product` (`pro_id`, `pro_name`, `pro_desc`, `cat_id`) VALUES
+(1, 'GTA V', 'Windows 7 and above with i5 processor and 8GB RAM', 2),
+(2, 'TSHIRT', 'SIZE-L with Black, Blue and White variations', 5),
+(3, 'ROG LAPTOP', 'Windows 10 with 15inch screen, i7 processor, 1TB S', 4),
+(4, 'OATS', 'Highly Nutritious from Nestle', 3),
+(5, 'HARRY POTTER', 'Best Collection of all time by J.K Rowling', 1),
+(6, 'MILK', '1L Toned MIlk', 3),
+(7, 'Boat EarPhones', '1.5Meter long Dolby Atmos', 4),
+(8, 'Jeans', 'Stretchable Denim Jeans with various sizes and col', 5),
+(9, 'Project IGI', 'compatible with windows 7 and above', 2),
+(10, 'Hoodie', 'Black GUCCI for 13 yrs and above', 5),
+(11, 'Rich Dad Poor D', 'Written by RObert Kiyosaki', 1),
+(12, 'Train Your Brain', 'By Shireen Stephen', 1);
 
-insert into `order` values(20, 1500, "2021-10-12", 3, 5);
-insert into `order` values(25, 30500, "2021-09-16", 5, 2);
-insert into `order` values(26, 2000, "2021-10-05", 1, 1);
-insert into `order` values(30, 3500, "2021-08-16", 4, 3);
-insert into `order` values(50, 2000, "2021-10-06", 2, 1);
+INSERT INTO `supplier_pricing` (`pricing_id`, `pro_id`, `supp_id`, `supp_price`) VALUES
+(1, 1, 2, 1500),
+(2, 3, 5, 30000),
+(3, 5, 1, 3000),
+(4, 2, 3, 2500),
+(5, 4, 1, 1000),
+(6, 12, 2, 780),
+(7, 12, 4, 789),
+(8, 3, 1, 31000),
+(9, 1, 5, 1450),
+(10, 4, 2, 999),
+(11, 7, 3, 549),
+(12, 7, 4, 529),
+(13, 6, 2, 105),
+(14, 6, 1, 99),
+(15, 2, 5, 2999),
+(16, 5, 2, 2999);
 
-insert into `rating` values(1, 2, 2, 4);
-insert into `rating` values(2, 3, 4, 3);
-insert into `rating` values(3, 5, 1, 5);
-insert into `rating` values(4, 1, 3, 2);
-insert into `rating` values(5, 4, 5, 4);
+INSERT INTO `orders` (`ord_id`, `ord_date`, `cus_id`, `pricing_id`) VALUES
+(101, '2021-10-06', 2, 1),
+(102, '2021-10-12', 3, 5),
+(103, '2021-09-16', 5, 2),
+(104, '2021-10-05', 1, 1),
+(105, '2021-08-16', 4, 3),
+(106, '2021-08-18', 1, 9),
+(107, '2021-09-01', 3, 7),
+(108, '2021-09-07', 5, 6),
+(109, '2021-09-10', 5, 3),
+(110, '2021-09-10', 2, 4),
+(111, '2021-09-15', 4, 5),
+(112, '2021-09-16', 4, 7),
+(113, '2021-09-16', 1, 8),
+(114, '2021-09-16', 3, 5),
+(115, '2021-09-16', 5, 3),
+(116, '2021-09-17', 2, 14);
+
+insert into 'rating' ('rat_id')
 
 /*---------------------------------------------------------------------------*/
 /* 3. Display the number of the customer group by their genders who have 
