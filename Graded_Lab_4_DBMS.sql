4c4de90c8b7b84ab93ad233f3da21cea9a808eab
Swati Beniwal <101699580+SwatiBen@users.noreply.github.com>
2023-08-15 22:00:02 +0530

    Update Graded_Lab_4_DBMS.sql
    done

--- Graded_Lab_4_DBMS.sql
+++ Graded_Lab_4_DBMS.sql
@@ -1,16 +1,15 @@
-/*---------------------------------------------------------------------------*/
-/* 1 Create the Database and the tables. */
-/*---------------------------------------------------------------------------*/
+/* 1 Create the tables. */
+
 Create Database if not exists `ecomm_DBMS` ;
 use `ecomm_DBMS`;
-/* Supplier Scema */
+
 CREATE TABLE IF NOT EXISTS `supplier`(
 `SUPP_ID` INT PRIMARY KEY,
 `SUPP_NAME` VARCHAR(50),
 `SUPP_CITY` VARCHAR(50),
 `SUPP_PHONE` VARCHAR(10)
 );
-/* Customer Scema */
+
 CREATE TABLE IF NOT EXISTS `customer` (
 `CUS_ID` INT NOT NULL,
 `CUS_NAME` VARCHAR(20) NULL DEFAULT NULL,
@@ -19,13 +18,13 @@ CREATE TABLE IF NOT EXISTS `customer` (
 `CUS_GENDER` CHAR,
 PRIMARY KEY (`CUS_ID`)
 );
-/* Product category schema */
+
 CREATE TABLE IF NOT EXISTS `category` (
 `CAT_ID` INT NOT NULL,
 `CAT_NAME` VARCHAR(20) NULL DEFAULT NULL,
 PRIMARY KEY (`CAT_ID`)
 );
-/* Product Schema */
+
 CREATE TABLE IF NOT EXISTS `product` (
 `PRO_ID` INT NOT NULL,
 `PRO_NAME` VARCHAR(20) NULL DEFAULT NULL,
@@ -34,8 +33,8 @@ CREATE TABLE IF NOT EXISTS `product` (
 PRIMARY KEY (`PRO_ID`),
 FOREIGN KEY (`CAT_ID`) REFERENCES category (`CAT_ID`)
 );
-/* Product details schema */
-CREATE TABLE IF NOT EXISTS `productDetails` (
+
+CREATE TABLE IF NOT EXISTS `supplier_pricing` (
 `PROD_ID` INT NOT NULL,
 `PRO_ID` INT NOT NULL,
 `SUPP_ID` INT NOT NULL,
@@ -44,7 +43,7 @@ PRIMARY KEY (`PROD_ID`),
 FOREIGN KEY (`PRO_ID`) REFERENCES product (`PRO_ID`),
 FOREIGN KEY (`SUPP_ID`) REFERENCES supplier(`SUPP_ID`)
 );
-/* Order details schema */
+
 CREATE TABLE IF NOT EXISTS `order` (
 `ORD_ID` INT NOT NULL,
 `ORD_AMOUNT` INT NOT NULL,
@@ -53,9 +52,9 @@ CREATE TABLE IF NOT EXISTS `order` (
 `PROD_ID` INT NOT NULL,
 PRIMARY KEY (`ORD_ID`),
 FOREIGN KEY (`CUS_ID`) REFERENCES customer(`CUS_ID`),
-FOREIGN KEY (`PROD_ID`) REFERENCES productDetails(`PROD_ID`)
+FOREIGN KEY (`PROD_ID`) REFERENCES supplier_pricing(`PROD_ID`)
 );
-/* product rating schema */
+
 CREATE TABLE IF NOT EXISTS `rating` (
 `RAT_ID` INT NOT NULL,
 `CUS_ID` INT NOT NULL,
@@ -65,9 +64,9 @@ PRIMARY KEY (`RAT_ID`),
 FOREIGN KEY (`SUPP_ID`) REFERENCES supplier (`SUPP_ID`),
 FOREIGN KEY (`CUS_ID`) REFERENCES customer(`CUS_ID`)
 );
-/*---------------------------------------------------------------------------*/
+
 /* 2 Insert values in all the tables */
-/*---------------------------------------------------------------------------*/
+
 insert into `supplier` values(1, "Rajesh Retails", "Delhi", "1234567890");
 insert into `supplier` values(2, "Appario Ltd.", "Mumbai", "2589631470");
 insert into `supplier` values(3, "Knome products", "Banglore", "9785462315");
@@ -92,11 +91,11 @@ insert into `product` values(3, "ROG LAPTOP", "DFNTTNTNTERND", 4);
 insert into `product` values(4, "OATS", "REURENTBTOTH", 3);
 insert into `product` values(5, "HARRY POTTER", "NBEMCTHTJTH", 1);
 
-insert into `productDetails` values(1, 1, 2, 1500);
-insert into `productDetails` values(2, 3, 5, 30000);
-insert into `productDetails` values(3, 5, 1, 3000);
-insert into `productDetails` values(4, 2, 3, 2500);
-insert into `productDetails` values(5, 4, 1, 1000);
+insert into `supplier_pricing` values(1, 1, 2, 1500);
+insert into `supplier_pricing` values(2, 3, 5, 30000);
+insert into `supplier_pricing` values(3, 5, 1, 3000);
+insert into `supplier_pricing` values(4, 2, 3, 2500);
+insert into `supplier_pricing` values(5, 4, 1, 1000);
 
 insert into `order` values(20, 1500, "2021-10-12", 3, 5);
 insert into `order` values(25, 30500, "2021-09-16", 5, 2);
@@ -122,32 +121,32 @@ group by customer.cus_gender;
 /* 4. Display all the orders along with the product name ordered by a customer 
 	  having Customer_Id=2. */
 /*---------------------------------------------------------------------------*/
-select `order`.*, product.pro_name from `order`, productDetails, product where 
-`order`.cus_id = 2 and `order`.prod_id = productDetails.prod_id and 
-productDetails.prod_id = product.pro_id;
+select `order`.*, product.pro_name from `order`, supplier_pricing, product where 
+`order`.cus_id = 2 and `order`.prod_id = supplier_pricing.prod_id and 
+supplier_pricing.prod_id = product.pro_id;
 
 /*---------------------------------------------------------------------------*/
 /* 5. Display the Supplier details who can supply more than one product*/
 /*---------------------------------------------------------------------------*/
-select supplier.* from supplier, productDetails where supplier.supp_id in 
-(select productDetails.supp_id from productDetails group by productDetails.supp_id
-having count(productDetails.supp_id)>1) group by supplier.supp_id;
+select supplier.* from supplier, supplier_pricing where supplier.supp_id in 
+(select supplier_pricing.supp_id from supplier_pricing group by supplier_pricing.supp_id
+having count(supplier_pricing.supp_id)>1) group by supplier.supp_id;
 
 /*---------------------------------------------------------------------------*/
 /* 6. Find the category of the product whose order amount is minimum. */
 /*---------------------------------------------------------------------------*/
-select category.* from `order`, productDetails, product, category
-where `order`.prod_id = productDetails.prod_id
-and productDetails.pro_id = product.pro_id
+select category.* from `order`, supplier_pricing, product, category
+where `order`.prod_id = supplier_pricing.prod_id
+and supplier_pricing.pro_id = product.pro_id
 and product.cat_id = category.cat_id
 and `order`.ord_amount = (SELECT MIN(ord_amount) FROM `order`);
 
 /*---------------------------------------------------------------------------*/
 /* 7. Display the Id and Name of the Product ordered after “2021-10-05”. */
 /*---------------------------------------------------------------------------*/
-select product.pro_id, product.pro_name from `order` inner join productDetails 
-on productDetails.prod_id = `order`.prod_id inner join product on 
-product.pro_id = productDetails.pro_id where `order`.ord_date > "2021-10-05";
+select product.pro_id, product.pro_name from `order` inner join supplier_pricing 
+on supplier_pricing.prod_id = `order`.prod_id inner join product on 
+product.pro_id = supplier_pricing.pro_id where `order`.ord_date > "2021-10-05";
 
 /*---------------------------------------------------------------------------*/
 /* 8. Display customer name and gender whose names start or end with 
